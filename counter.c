0a031876f79f0976671783095ed95913115b2375
yangy393 <yangy393@pascal.cas.mcmaster.ca>
2021-04-13 10:44:41 -0400

    delete the way of finding max and min in origin in order to use the way of finding max and min in feature

--- counter.c
+++ counter.c
@@ -74,34 +74,14 @@ int main()
                               other,
                               (((float) count[i]) / strlen(buffer)) * 100);
 
-  // Find the max and min occuring character in the string, in particular the
-  // position in the count array of each character
-  int max, min, max_pos, min_pos;
-  max = min = count[0];
-  min_pos = max_pos = 0;
-  for (i = 0; i < COUNT_SIZE; i++) {
-    if (count[i] < min)
-    {
-      min_pos = i;
-      min = count[i];
-    }
-    if (count[i] > max)
-    {
-      max_pos = i;
-      max = count[i];
-    }
-  }
 
-  // Output the max and min occuring letter, again using 65 as an offset to
-  // output the character character given ASCII A-Z range from 65-90
-  printf("\nThe most frequently occurring letter is %c.\n", max_pos + 65);
-  printf("The least frequently occurring letter is %c.\n", min_pos + 65);
-
-  return 0;
 }
 =======
   printf("\nTotal spaces: %d\n", spaces);
-
+  // Output the number of other characters
+  printf("%-10s%-15d%-15.2f\n","Other",
+                              other,
+                              (((float) count[i]) / strlen(buffer)) * 100);
 
   // Find the max and min occuring character in the string, in particular the
   // position in the count array of each character
